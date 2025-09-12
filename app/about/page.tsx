c8e1ee80c2056f5f639c39b67cb5db70f187d203
windowwelder <welderules@duck.com>
2025-09-12 18:45:19 +0700

    feat: initial structure of about page

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+export default  function AboutPage() {
+    return (
+        <section className="aboutpage-container">
+            <img src="/hero-image-square"/>
+            <div className="aboutpage-text">
+            <h2>About printforge</h2>
+            <h1>Empowering makers worldwide</h1>
+            <p>
+                Founded in 2023, PrintForge has quickly become the go-to platform for 3D printing enthusiasts, makers, and professional designers to share and discover amazing STL files for 3D printing.
+
+                Our mission is to foster a vibrant community where creativity meets technology, enabling anyone to bring their ideas to life through 3D printing.
+            </p>
+            </div>
+        </section>
+    )
+}
