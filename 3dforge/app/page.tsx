c74750aaa6e68bc629569bea3f148d03f40641c3
windowwelder <welderules@duck.com>
2025-09-12 18:09:12 +0700

    initial structure for the home page

--- 3dforge/app/page.tsx
+++ 3dforge/app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return;
+  return  (<div className="Homepage-container">
+                <div className="homepage-text-and-button">
+                <p>
+                  <span>Your go-to platform for 3D printing files</span>
+                  <span>Discover what’s possible with 3D printing</span>
+                  <span>Join our community of creators and explore a vast library of user-submitted models.</span>
+                  <button>Browse models</button>
+                </p>
+                </div>
+                <img src=""/>
+          </div>)
 }
