f7e00d253fba6cfa2b8303c8ae8b1878dca17ede
windowwelder <welderules@duck.com>
2025-09-12 18:19:14 +0700

    chore: cleaning up the project directory

--- 3dforge/app/page.tsx
+++ 3dforge/app/page.tsx
@@ -1,15 +1,14 @@
-import Image from "next/image";
 
 export default function Home() {
   return  (<div className="Homepage-container">
-                <div className="homepage-text-and-button">
+                <main className="homepage-text-and-button">
                 <p>
                   <span>Your go-to platform for 3D printing files</span>
                   <span>Discover what’s possible with 3D printing</span>
                   <span>Join our community of creators and explore a vast library of user-submitted models.</span>
                   <button>Browse models</button>
                 </p>
-                </div>
-                <img src=""/>
+                <img src="/home-page-hero-img.png"/>
+                </main>
           </div>)
 }
--- app/products/page.jsx
+++
@@ -1,3 +0,0 @@
-export default function ProductsPage() {
-    return <h1>This is the products page</h1>
-}
