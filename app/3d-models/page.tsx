22d1e6b51edb1eab0f14569f31297e4742f84203
windowwelder <welderules@duck.com>
2025-09-23 13:41:42 +0700

    refactor: nextjs Form component

--- app/3d-models/page.tsx
+++ app/3d-models/page.tsx
@@ -1,6 +1,7 @@
 import ModelsGrid from "@/app/components/ModelsGrid"
 import type { ModelsPageProps } from "@/app/types"
 import { getModels } from "@/app/lib/models"
+import Form from "next/form"
 
 
 export default async function Page({ searchParams }: ModelsPageProps) {
@@ -16,16 +17,15 @@ export default async function Page({ searchParams }: ModelsPageProps) {
 
     return (
         <>
-            <form className="w-full px-5 md:px-0 md:max-w-xl">
+            <Form action="/3d-models">
                 <input
                     type="text"
                     name="query"
                     placeholder="E.g. dragon"
                     autoComplete="off"
                     defaultValue={query}
-                    className="w-full py-3 pl-5 pr-5 text-sm placeholder-gray-500 bg-white border border-[#606060] rounded-full focus:border-[#606060] focus:outline-none focus:ring-0 md:text-base"
                 />
-            </form>
+            </Form>
             <ModelsGrid title="3D Models" models={filteredModels} />
         </>
     )
